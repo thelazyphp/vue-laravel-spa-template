dcd446bd78e0a819e837df62e6850f7491de691f
The Lazy PHP <56831662+thelazyphp@users.noreply.github.com>
2019-11-19 18:55:29 +0300

    Some fixes

---
+++ src/resources/js/components/FooterLanguageSelect.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="dropup">
+        <a
+            id="dropdownMenuButton"
+            href="#"
+            class="dropdown-toggle"
+            role="button"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+        >
+            {{ $t('language', $i18n.locale) }}
+        </a>
+
+        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
+            <a
+                v-for="locale in locales"
+                :key="locale"
+                href="#"
+                class="dropdown-item"
+                :class="{ 'active': locale == $i18n.locale }"
+                @click="$i18n.locale = locale"
+            >
+                {{ $t('language', locale) }}
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        computed: {
+            locales () {
+                return Object.keys(this.$i18n.messages)
+            }
+        }
+    }
+</script>
--- src/resources/js/components/TheFooter.vue
+++ src/resources/js/components/TheFooter.vue
@@ -1,11 +1,21 @@
 <template>
     <footer>
-        <div class="text-center text-muted">&copy; {{ year }}</div>
+        <ul class="list-inline text-center text-muted">
+            <li class="list-inline-item">&copy; {{ year }}</li>
+
+            <li class="list-inline-item">
+                <FooterLanguageSelect/>
+            </li>
+        </ul>
     </footer>
 </template>
 
 <script>
+    import FooterLanguageSelect from './FooterLanguageSelect'
+
     export default {
+        components: { FooterLanguageSelect },
+
         computed: {
             year () {
                 return (new Date()).getFullYear()
