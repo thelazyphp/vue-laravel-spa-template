b69fd0e5106bbba55f061869d05896b1566982e3
The Lazy PHP <56831662+thelazyphp@users.noreply.github.com>
2020-05-26 14:42:59 +0300

    Update Parser.php

--- src/backend/Parsing/Parser.php
+++ src/backend/Parsing/Parser.php
@@ -2,8 +2,11 @@
 
 namespace App\Parsing;
 
-use GuzzleHttp\Client;
 use function str_get_html;
+
+use Exception;
+use GuzzleHttp\Client;
+use Illuminate\Support\Collection;
 
 class Parser
 {
@@ -11,6 +14,11 @@ class Parser
      * @var string
      */
     protected $userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36';
+
+    /**
+     * @var string
+     */
+    protected $model;
 
     /**
      * @var Client
@@ -31,6 +39,77 @@ class Parser
     }
 
     /**
+     * @return void
+     */
+    public function run()
+    {
+        set_time_limit(0);
+
+        $links = $this->parseLinks();
+
+        if (
+            !is_array($links)
+            && !($links instanceof Collection)
+        ) {
+            throw new Exception(
+                'Methd [parseLinks] must return an array or an instance of [\Illuminate\Support\Collection]!'
+            );
+        }
+
+        if (is_array($links)) {
+            $links = new Collection($links);
+        }
+    }
+
+    /**
+     * @return Collection|string[]
+     */
+    protected function parseLinks()
+    {
+        return [];
+    }
+
+    /**
+     * @param  string  $url
+     * @return simple_html_dom
+     */
+    protected function getHtml($url)
+    {
+        return str_get_html(
+            $this->getPage($url, [
+                'headers' => [
+                    'accept' => 'text/html',
+                ],
+            ])
+        );
+    }
+
+    /**
+     * @param  string  $url
+     * @return object
+     */
+    protected function getJson($url)
+    {
+        return json_decode(
+            $this->getPage($url, [
+                'headers' => [
+                    'accept' => 'application/json',
+                ],
+            ])
+        );
+    }
+
+    /**
+     * @param  string  $url
+     * @param  array  $options
+     * @return string
+     */
+    protected function getPage($url, $options = [])
+    {
+        return (string) $this->httpClient->get($url, $options)->getBody();
+    }
+
+    /**
      * @return Client
      */
     protected function makeHttpClient()
@@ -44,40 +123,5 @@ class Parser
         return new Client(
             array_merge($config, $this->httpClientConfig)
         );
-    }
-
-    /**
-     * @param  string  $url
-     * @return object
-     */
-    protected function getJson($url)
-    {
-        return json_decode(
-            $this->getPage($url)
-        );
-    }
-
-    /**
-     * @param  string  $url
-     * @return simple_html_dom
-     */
-    protected function getHtml($url)
-    {
-        return str_get_html(
-            $this->getPage($url)
-        );
-    }
-
-    /**
-     * @param  string  $url
-     * @return string
-     */
-    protected function getPage($url)
-    {
-        if (!($this->httpClient instanceof Client)) {
-            $this->httpClient = $this->makeHttpClient();
-        }
-
-        return (string) $this->httpClient->get($url)->getBody();
     }
 }
