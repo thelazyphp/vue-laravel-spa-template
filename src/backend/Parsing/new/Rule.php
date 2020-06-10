4d0fbe7fc73ddacd189ced1a28b99a08e34f24db
The Lazy PHP <56831662+thelazyphp@users.noreply.github.com>
2020-06-10 15:36:56 +0300

    Update Rule.php

--- src/backend/Parsing/new/Rule.php
+++ src/backend/Parsing/new/Rule.php
@@ -2,15 +2,25 @@
 
 namespace App\Parsing;
 
+use Illuminate\Support\Collection;
+use Closure;
+use simple_html_dom;
+use simple_html_dom_node;
+
 /**
  *
  */
 class Rule
 {
     /**
-     * @var array
+     * @var \Illuminate\Support\Collection
      */
-    protected $actions = [];
+    protected $cache;
+
+    /**
+     * @var \Closure[]
+     */
+    protected $closures = [];
 
     /**
      *
@@ -21,14 +31,6 @@ class Rule
     }
 
     /**
-     * @return array
-     */
-    public function actions()
-    {
-        return $this->actions;
-    }
-
-    /**
      * @param string $selector
      * @param int|null $index
      *
@@ -36,22 +38,8 @@ class Rule
      */
     public function find($selector, $index = 0)
     {
-        $type = 'find';
-
-        $this->actions[] = compact(
-            'type', 'selector', 'index'
-        );
-
+        $this->closures[] = $this->getFindClosure($selector, $index);
         return $this;
-    }
-
-    /**
-     * @param string $selector
-     * @return self
-     */
-    public function findAll($selector)
-    {
-        return $this->find($selector, null);
     }
 
     /**
@@ -65,20 +53,38 @@ class Rule
     public function findWithText(
         $selector,
         $value,
-        $index = 0,
-        $ignoreCase = false
-    ) {
-        $type = 'findWithText';
-        $cond = 'equals';
+        $index,
+        $ignoreCase = false)
+    {
+        $this->closures[] = function ($res) use (
+            $selector,
+            $value,
+            $index,
+            $ignoreCase)
+        {
+            if ($res instanceof simple_html_dom || $res instanceof simple_html_dom_node) {
+                $func = $ignoreCase ? 'strcasecmp' : 'strcmp';
+                $found = [];
 
-        $this->actions[] = compact(
-            'type',
-            'cond',
-            'selector',
-            'value',
-            'inxdex',
-            'ignoreCase'
-        );
+                foreach ($this->getFindClosure($selector)($res) as $node) {
+                    $text = trim($node->plaintext);
+
+                    $res = call_user_func(
+                        $func,
+                        $text,
+                        $value
+                    );
+
+                    if ($res === 0) {
+                        $found[] = $node;
+                    }
+                }
+
+                return $this->take($found, $index);
+            }
+
+            return $res;
+        };
 
         return $this;
     }
@@ -94,20 +100,38 @@ class Rule
     public function findWithTextHas(
         $selector,
         $value,
-        $index = 0,
-        $ignoreCase = false
-    ) {
-        $type = 'findWithText';
-        $cond = 'has';
+        $index,
+        $ignoreCase = false)
+    {
+        $this->closures[] = function ($res) use (
+            $selector,
+            $value,
+            $index,
+            $ignoreCase)
+        {
+            if ($res instanceof simple_html_dom || $res instanceof simple_html_dom_node) {
+                $func = $ignoreCase ? 'mb_stripos' : 'mb_strpos';
+                $found = [];
 
-        $this->actions[] = compact(
-            'type',
-            'cond',
-            'selector',
-            'value',
-            'inxdex',
-            'ignoreCase'
-        );
+                foreach ($this->getFindClosure($selector)($res) as $node) {
+                    $text = trim($node->plaintext);
+
+                    $res = call_user_func(
+                        $func,
+                        $text,
+                        $value
+                    );
+
+                    if ($res !== false) {
+                        $found[] = $node;
+                    }
+                }
+
+                return $this->take($found, $index);
+            }
+
+            return $res;
+        };
 
         return $this;
     }
@@ -123,20 +147,38 @@ class Rule
     public function findWithTextStarts(
         $selector,
         $value,
-        $index = 0,
-        $ignoreCase = false
-    ) {
-        $type = 'findWithText';
-        $cond = 'starts';
+        $index,
+        $ignoreCase = false)
+    {
+        $this->closures[] = function ($res) use (
+            $selector,
+            $value,
+            $index,
+            $ignoreCase)
+        {
+            if ($res instanceof simple_html_dom || $res instanceof simple_html_dom_node) {
+                $func = $ignoreCase ? 'mb_stripos' : 'mb_strpos';
+                $found = [];
 
-        $this->actions[] = compact(
-            'type',
-            'cond',
-            'selector',
-            'value',
-            'inxdex',
-            'ignoreCase'
-        );
+                foreach ($this->getFindClosure($selector)($res) as $node) {
+                    $text = trim($node->plaintext);
+
+                    $res = call_user_func(
+                        $func,
+                        $text,
+                        $value
+                    );
+
+                    if ($res === 0) {
+                        $found[] = $node;
+                    }
+                }
+
+                return $this->take($found, $index);
+            }
+
+            return $res;
+        };
 
         return $this;
     }
@@ -152,21 +194,85 @@ class Rule
     public function findWithTextEnds(
         $selector,
         $value,
-        $index = 0,
-        $ignoreCase = false
-    ) {
-        $type = 'findWithText';
-        $cond = 'ends';
+        $index,
+        $ignoreCase = false)
+    {
+        $this->closures[] = function ($res) use (
+            $selector,
+            $value,
+            $index,
+            $ignoreCase)
+        {
+            if ($res instanceof simple_html_dom || $res instanceof simple_html_dom_node) {
+                $func = $ignoreCase ? 'mb_stripos' : 'mb_strpos';
+                $found = [];
 
-        $this->actions[] = compact(
-            'type',
-            'cond',
-            'selector',
-            'value',
-            'inxdex',
-            'ignoreCase'
-        );
+                foreach ($this->getFindClosure($selector)($res) as $node) {
+                    $text = trim($node->plaintext);
+                    $pos = mb_strlen($text) - mb_strlen($value);
+
+                    $res = call_user_func(
+                        $func,
+                        $text,
+                        $value
+                    );
+
+                    if ($res === $pos) {
+                        $found[] = $node;
+                    }
+                }
+
+                return $this->take($found, $index);
+            }
+
+            return $res;
+        };
 
         return $this;
+    }
+
+    /**
+     * @param \simple_html_dom[]|\simple_html_dom_node[] $nodes
+     * @param int|null $index
+     *
+     * @return \simple_html_dom_node[]|simple_html_dom_node|null
+     */
+    protected function take($nodes, $index)
+    {
+        if (is_null($index)) {
+            return $nodes;
+        }
+
+        if ($index < 0) {
+            $index += count($nodes);
+        }
+
+        return isset($nodes[$index]) ? $nodes[$index] : null;
+    }
+
+    /**
+     * @param string $selector
+     * @param int|null $index
+     *
+     * @return \Closure
+     */
+    protected function getFindClosure($selector, $index = null)
+    {
+        return function ($res) use ($selector, $index) {
+            if ($res instanceof simple_html_dom || $res instanceof simple_html_dom_node) {
+                if (!$this->cache->has($selector)) {
+                    $res = $res->find($selector, $index);
+
+                    if (!is_null($res)) {
+                        $this->cache->put($selector, $res);
+                    }
+                } else {
+                    $res = $this->cache->get($selector);
+                    return is_array($res) ? $this->take($res, $index) : $res;
+                }
+            }
+
+            return $res;
+        };
     }
 }
